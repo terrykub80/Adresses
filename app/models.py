aec4bcbbac8dde1d7040d4d73f9db4f6fe5874a0
Terry Kubalewski <terrykub@yahoo.com>
2023-01-12 09:35:20 -0600

    updated Address class

--- app/models.py
+++ app/models.py
@@ -27,11 +27,18 @@ class Address(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     phone_number = db.Column(db.String(50), nullable=False)
-    address = db.Column(db.String(200), nullable=False)
+    street_address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(20), nullable=False)
+    zip_code = db.Column(db.String(10), nullable=False)
+    address = f"{street_address}, {city}, {state} {zip_code}"
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         db.session.add(self)
         db.session.commit()
+
+        def __repr__(self):
+            return f"<User ID: {self.id} | Address: {self.address}>"
         
